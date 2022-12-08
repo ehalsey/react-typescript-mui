6c2a1f4a6c2485c117dd53f4eab347af532c05a3
Eric Halsey <ehalseyhb@gmail.com>
2022-12-08 13:52:55 -0800

    working

--- src/BasicUsage.tsx
+++ src/BasicUsage.tsx
@@ -1,13 +1,37 @@
+import { InputLabel, MenuItem, Select } from '@mui/material';
 import { styled } from '@mui/system';
+import React from 'react';
 
 const MyComponent = styled('select')({
   height: '36px',
   border: '1px solid rgba(34, 27, 78, 0.5)',
   color: '#000000',
   borderRadius: '8px',
-  paddingLeft: '16px',
+  paddingLeft: '16px'
 });
 
 export default function BasicUsage() {
-  return <MyComponent><option>one</option></MyComponent>;
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+  }
+  const [age, setAge] = React.useState(0);
+  
+  return <><MyComponent onChange={() => console.log("here1")}>
+    <option>one</option>
+    <option>two</option>
+    <option>three</option>
+  </MyComponent>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={age}
+    label="Age"
+    onChange={event => {console.log('here');setAge(event.target.value as number) as void}}
+  >
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
+  </>;
 }
