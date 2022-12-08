d5ae35a47fcd629e1d889de5457c6d01920b1410
Eric Halsey <ehalseyhb@gmail.com>
2022-12-08 14:58:46 -0800

    really working

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 export default function Form() {
 
